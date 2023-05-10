03b36ab00a0e2c389ebd3a02087d2fc6635d4bd9
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2023-05-11 00:07:57 +0300

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DatabaseConfiguration } from './database/database-configuration';
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			useClass: DatabaseConfiguration,
+		}),
+	],
 })
 export class AppModule {}
