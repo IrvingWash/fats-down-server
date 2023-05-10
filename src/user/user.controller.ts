299b807e6a4a2eda65bddef0eb59788a45c5a685
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2023-05-11 01:55:15 +0300

    add get all users endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	ClassSerializerInterceptor,
+	Controller,
+	Get,
+	UseInterceptors,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
 @Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
 	public constructor(
 		private readonly _userService: UserService
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,18 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { todo } from 'src/utils/helpers';
-
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
 	public constructor(
 		@InjectRepository(User)
-		private readonly _userEntity: Repository<User>
+		private readonly _userRepository: Repository<User>
 	) {}
 
 	public async getAllUsers(): Promise<User[]> {
-		todo();
+		return this._userRepository.find();
 	}
 }
