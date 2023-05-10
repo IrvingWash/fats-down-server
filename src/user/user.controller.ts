b6df1787fb1be9ce4fc6e5497bd82115ec0a5981
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2023-05-11 02:02:22 +0300

    guar get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,8 +2,13 @@ import {
 	ClassSerializerInterceptor,
 	Controller,
 	Get,
+	Req,
 	UseInterceptors,
 } from '@nestjs/common';
+
+import { Request } from 'express';
+
+import { ITokenParser, TokenParser } from 'src/auth/token-parser';
 
 import { UserService } from './user.service';
 import { User } from './user.entity';
@@ -11,12 +16,21 @@ import { User } from './user.entity';
 @Controller('user')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
+	private readonly _tokenParser: ITokenParser;
+
 	public constructor(
 		private readonly _userService: UserService
-	) {}
+	) {
+		this._tokenParser = new TokenParser();
+	}
 
 	@Get()
-	public getAllUsers(): Promise<User[]> {
-		return this._userService.getAllUsers();
+	public getAllUsers(
+		@Req()
+			request: Request
+	): Promise<User[]> {
+		const accessToken = this._tokenParser.getAccessToken(request);
+
+		return this._userService.getAllUsers(accessToken);
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
+import { TokenService } from 'src/auth/token.service';
 
 @Injectable()
 export class UserService {
 	public constructor(
 		@InjectRepository(User)
-		private readonly _userRepository: Repository<User>
+		private readonly _userRepository: Repository<User>,
+
+		private readonly _tokenService: TokenService
 	) {}
 
-	public async getAllUsers(): Promise<User[]> {
+	public async getAllUsers(accessToken: string): Promise<User[]> {
+		await this._getUser(accessToken);
+
 		return this._userRepository.find();
+	}
+
+	private async _getUser(accessToken: string): Promise<User> {
+		const validationResult = this._tokenService.validateAccessToken(accessToken);
+
+		const user = await this._userRepository.findOneBy({
+			username: validationResult.username,
+		});
+
+		if (user === null) {
+			throw new UnauthorizedException();
+		}
+
+		return user;
 	}
 }
